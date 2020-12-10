8d6ab7dcd54b19006346f4ab5fd2ece9cb3b6adb
Belén MLT <belenmlt@hotmail.com>
2020-12-10 10:05:17 +0100

    Model creación de usuarios.

---
+++ src/models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    city: String,
+    province: String,
+    zipCode: Number,
+    // conditions: Boolean
+
+});
+
+module.exports = mongoose.model("User", UserSchema);
+
