64f3db6b5091fffd1ec43813ba51ae17e7492cfd
Belén MLT <belenmlt@hotmail.com>
2020-12-11 11:47:55 +0100

    Controlador reservas de productos/mensajes añadido.

---
+++ src/controllers/product-booking.controller.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const ProductBooking = require("../models/product-booking.model");
+
+// mongoose.connect('mongodb+srv://belenmlt:%AG0qUfcY@DL@cluster0.xq5kj.mongodb.net/Alquilator?retryWrites=true&w=majority', 
+//                 { useNewUrlParser: true, useUnifiedTopology: true }
+//                 );
+
+
+class ProductBookingController {
+    public async getBookings(req: Request, res: Response){
+        try {
+            const allMessages = await ProductBooking.find();
+            res.send(allMessages);
+
+        } catch(error) {
+            console.log(error);
+            res.sendStatus(404);
+        }
+    };
+
+    public async postBookings(req: Request, res: Response){
+        try {
+            // const sendMessage = req.body;
+            // const newMessage = new ProductBooking();
+            // newMessage.message = sendMessage.message;
+
+            const message = {
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                message: req.body.message,
+                productName: req.body.productName
+            };
+            
+            const newMessage = new ProductBooking();
+
+            newMessage.name = message.name;
+            newMessage.email = message.email;
+            newMessage.phone = message.phone;
+            newMessage.message = message.message;
+            newMessage.productName = message.productName;
+
+            await newMessage.save();
+
+            res.send(newMessage);
+
+        } catch(error) {
+            console.log(error);
+            res.sendStatus(404);
+        }
+        
+    };
+
+    public async deleteBookings(req: Request, res: Response){
+        try {
+            const deleteMessage = req.params.id;
+            const response = await ProductBooking.deleteOne({_id: deleteMessage});
+
+            res.send("El mensaje se ha borrado correctamente.");
+
+        } catch(error) {
+            console.log(error);
+            res.sendStatus(400);
+        }
+    };
+
+    public async updateBookings(req: Request, res: Response){
+        try {
+            const modifyMessage = req.body;
+            const response = await ProductBooking.updateOne({_id: modifyMessage._id}, {message: modifyMessage.message});
+                            // {busca el mensaje con id (el propio que se le da alguardaro en mongoAtlas)...}, {modifica el mensaje del productBooking que tiene id x...}
+
+            res.send(`El mensaje enviado al propietario ha sido modificado.`);
+
+        } catch(error) {
+            console.log(error);
+            res.sendStatus(404);
+        }
+    };
+}
+
+export const productBookingController = new ProductBookingController();
